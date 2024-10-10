34cb6a53ff92d414b2f67a5ac0e1e61edcc3c4a7
Mabel <luccadacambra@gmail.com>
2024-10-10 12:15:33 -0400

    Time-linked objects

---
+++ Game/Intermediate/Build/Win64/x64/GameEditor/Development/Core/SharedPCH.Core.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/mabel/UnrealProjects/Time-Fire-game-GDW/Game/Intermediate/Build/Win64/x64/GameEditor/Development/Core/SharedDefinitions.Core.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
---
+++ Game/Intermediate/Build/Win64/x64/GameEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
+#include "C:/Users/mabel/UnrealProjects/Time-Fire-game-GDW/Game/Intermediate/Build/Win64/x64/GameEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
+#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
